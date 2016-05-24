c5d90aa3bbe9c77258aa5926f20e213dc8b4bb08
Satit Rianpit <siteslave@msn.com>
2016-05-24 14:25:17 +0700

    initial dashboard

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,31 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+  .controller('DashCtrl', function ($scope, $log, $ionicPlatform, $rootScope, UserService) {
+
+    $scope.users = [];
+
+    $scope.getUsers = function () {
+      $ionicPlatform.ready(function () {
+        UserService.getUsers($rootScope.db)
+          .then(function (res) {
+            for (var i = 0; i <= res.rows.length - 1; i++) {
+              $log.info(res.rows.item(i));
+              var obj = {};
+              obj.fullname = res.rows.item(i).fullname;
+              obj.position = res.rows.item(i).position;
+              obj.hospital = res.rows.item(i).hospital;
+
+              $scope.users.push(obj);
+            }
+          }, function (err) {
+            alert(JSON.stringify(err));
+          });
+      });
+    };
+
+    $scope.getUsers();
+
+  })
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
